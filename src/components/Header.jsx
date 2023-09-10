f201680a673b07f10e92238d1eca87d50acc3817
Wolfenstein <renzasdf@gmail.com>
2023-09-11 00:13:33 +0800

    Fix Cart Toggler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom';
 import { useContext, useMemo } from 'react';
 import { ShopContext } from '../App';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Header() {
-  const { addCart } = useContext(ShopContext);
+  const {
+    addCart,
+    decreaseCartQuantity,
+    increaseCartQuantity,
+    checkOut,
+    removeItem,
+  } = useContext(ShopContext);
 
   const isInCart = addCart.length > 0;
 
@@ -16,17 +23,24 @@ export default function Header() {
     );
   }, [addCart]);
 
+  const totalPrice = useMemo(() => {
+    return addCart.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    );
+  }, [addCart]);
+
   return (
     <>
       <nav className='custom-nav | navbar navbar-expand-lg bg-success-subtle text-success px-4'>
         <Link className='link link-success' to='/'>
-          <span className='navbar-brand text-success fs-3 fw-bold'>
+          <span className='navbar-brand text-success fs-2 fw-bold'>
             ShopLyft
           </span>
         </Link>
 
         <Link
-          className='custom-end | link link-success display fs-3'
+          className='custom-end | link link-success display fs-2 fw-bold'
           to='/Cart'>
           <ShoppingCartIcon style={{ fontSize: '2rem' }} />
           {isInCart <= 0 ? (
@@ -36,7 +50,7 @@ export default function Header() {
           )}
         </Link>
         <button
-          className='btn text-success bg-success-subtle d-lg-none custom-end'
+          className='btn text-success bg-success-subtle d-lg-none fs-2 custom-end'
           type='button'
           data-bs-toggle='offcanvas'
           data-bs-target='#offcanvasResponsive'
@@ -50,13 +64,13 @@ export default function Header() {
         </button>
 
         <div
-          className='offcanvas-lg offcanvas-end'
+          className='offcanvas-lg offcanvas-end canvas-display'
           tabIndex='-1'
           id='offcanvasResponsive'
           aria-labelledby='offcanvasResponsiveLabel'>
-          <div className='offcanvas-header'>
-            {addCart.length <= 0 ? (
-              <>
+          {addCart.length <= 0 ? (
+            <>
+              <div className='offcanvas-header'>
                 <span className='offcanvas-title text-success fw-bold'>
                   No items in your Cart
                 </span>
@@ -66,9 +80,11 @@ export default function Header() {
                   data-bs-dismiss='offcanvas'
                   data-bs-target='#offcanvasResponsive'
                   aria-label='Close'></button>
-              </>
-            ) : (
-              <>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='offcanvas-header'>
                 <span
                   className='offcanvas-title text-success fw-bold'
                   id='offcanvasResponsiveLabel'>
@@ -80,11 +96,68 @@ export default function Header() {
                   data-bs-dismiss='offcanvas'
                   data-bs-target='#offcanvasResponsive'
                   aria-label='Close'></button>
+              </div>
 
-                <div className='offcanvas-body'></div>
-              </>
-            )}
-          </div>
+              <div className='offcanvas-body bg-success-subtle p-0 container'>
+                {addCart.map((product) => {
+                  return (
+                    <div
+                      className='container p-0 my-3 d-flex bg-success'
+                      key={product.id}>
+                      <img
+                        src={product.imgURL}
+                        alt=''
+                        className='img-fluid h-50 w-50'
+                      />
+                      <div className='container d-flex flex-column mt-3 text-light'>
+                        <span className='fs-4 fw-bold text-light'>
+                          {product.title}
+                        </span>
+                        <span>Price: {product.price}</span>
+                        <span className='d-flex justify-content-between align-items-center'>
+                          <div className='col-6'>
+                            <span className=''>Qty: </span>
+                            <span className=' mx-1'>{product.quantity}</span>
+                          </div>
+                          <div className='d-flex col-6 justify-content-end'>
+                            <button
+                              className='btn text-light p-0 fs-4 mx-2'
+                              onClick={() => {
+                                increaseCartQuantity(product.id);
+                              }}>
+                              +
+                            </button>
+                            <button
+                              className='btn text-light p-0 fs-4 mx-2'
+                              onClick={() => {
+                                decreaseCartQuantity(product.id);
+                              }}>
+                              -
+                            </button>
+                          </div>
+                        </span>
+                        <div className='container p-0 h-100 d-flex align-items-end justify-content-end'>
+                          <button
+                            className='btn bg-success text-light p-0 mb-1'
+                            onClick={() => removeItem(product.id)}>
+                            <DeleteIcon />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                {/* <div className='custom-footer ps-1 bg-success-subtle text-success fw-bold'>
+                  <p>Total: {totalPrice.toFixed(2)}</p>
+                  <button
+                    className='btn fw-bold text-success'
+                    onClick={checkOut}>
+                    Check Out
+                  </button>
+                </div> */}
+              </div>
+            </>
+          )}
         </div>
       </nav>
     </>

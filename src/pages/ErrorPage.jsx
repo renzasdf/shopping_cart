d25c7630e70410da89c8484e2f02ad6085c0277d
Wolfenstein <renzasdf@gmail.com>
2023-09-13 15:27:51 +0800

    Fix Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 export default function ErrorPage() {
   return (
     <>
-      <h1>Error Page</h1>
-      <Link to='/'>
-        <h1>Click here to return!</h1>
-      </Link>
+      <div className='container-fluid d-flex flex-column justify-content-center align-items-center mt-5'>
+        <h1>Page not found!</h1>
+        <Link to='/'>
+          <h1>Click here to return!</h1>
+        </Link>
+      </div>
     </>
   );
 }
